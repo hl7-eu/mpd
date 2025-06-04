5d21c00f69bb415b1210dd8c017f1680e3c6b6b2
ncangigi <giorgio.cangioli@gmail.com>
2025-06-04 10:41:09 +0200

    1st ballot version

--- r4-r5-scripts/prepare-r5.bat
+++ r4-r5-scripts/prepare-r5.bat
@@ -1,5 +1,6 @@
 rmdir /S /Q ..\input\fsh\diff-fhir-rel
 xcopy /S /I /Y ..\input\diff\r5 ..\input\fsh\diff-fhir-rel
 copy sushi-config-r5.yaml ..\sushi-config.yaml
+copy publication-request-r5.json ..\publication-request.json
 copy ig-r5.ini ..\ig.ini
 REM copy alias-ig.r5 ..\input\fsh\alias-ig.fsh
